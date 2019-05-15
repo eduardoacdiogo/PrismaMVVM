f41961959378eb5a53579dc8e6007e0cc8b86df4
Eduardo Andre <educdiogo@hotmail.com>
2019-05-15 20:09:12 -0300

    Exemplo

---
+++ ProjetoPrismMVVM/ProjetoPrismMVVM/ProjetoPrismMVVM/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,54 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using ProjetoPrismMVVM.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjetoPrismMVVM.ViewModels
+{
+    public class MainPageViewModel : BindableBase
+    {
+        #region Global
+        INavigationService _navigationService;
+        #endregion
+
+        #region Binding
+
+        private string title;
+        public string Title
+        {
+            get { return title; }
+            set { SetProperty(ref title, value); }
+        }
+        #endregion
+
+        #region Command
+        public DelegateCommand ClickNavigationCommand { get; set; }
+
+        #endregion
+
+        public MainPageViewModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+            Title = "MainPage";
+            ClickNavigationCommand = new DelegateCommand(ExecuteClickNavigationCommand);
+        }
+
+        private void ExecuteClickNavigationCommand()
+        { 
+            Pessoa p = new Pessoa
+            {
+                ID = 1,
+                Name = "Nome"
+            };
+            var item = new NavigationParameters()
+            {
+                { "itemNav", p }
+            };
+            _navigationService.NavigateAsync("SecondPage",item);
+        }
+    }
+}
---
+++ ProjetoPrismMVVM/ProjetoPrismMVVM/ProjetoPrismMVVM/ViewModels/SecondPageViewModel.cs
@@ -0,0 +1,43 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using ProjetoPrismMVVM.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProjetoPrismMVVM.ViewModels
+{
+    public class SecondPageViewModel : BindableBase, INavigationAware
+    {
+        private Pessoa p;
+        public Pessoa P
+        {
+            get { return p; }
+            set {SetProperty(ref p, value); }
+        }
+
+        public SecondPageViewModel()
+        {
+
+        }
+
+        public void OnNavigatedFrom(INavigationParameters parameters)
+        {
+           
+        }
+
+        public void OnNavigatedTo(INavigationParameters parameters)
+        {
+            
+        }
+
+        public void OnNavigatingTo(INavigationParameters parameters)
+        {
+            if (parameters.ContainsKey("itemNav"))
+            {
+                P = (Pessoa)parameters["itemNav"];
+            }
+        }
+    }
+}
